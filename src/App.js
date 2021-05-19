a59faa2557943f92e43faa3cd5720108fea3af0f
KT <au637855@uni.au.dk>
2021-05-19 14:11:57 +0200

    made stuff work

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react'
-import './App.css'
-
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import './App.css'
 import Dashboard from './components/Dashboard/Dashboard'
 import Login from './components/Login/Login'
 import Preferences from './components/Preferences/Preferences'
+import useToken from './components/App/useToken';
 
 function App() {
-  const [token, setToken] = useState()
+  const { token, setToken } = useToken()
 
   if (!token) {
     return <Login setToken={setToken} />
